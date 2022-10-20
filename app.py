235020679b7cabae9b9d55fbec5c1fd068d5902b
karolkisinsi <karol.kisinski@wp.pl>
2022-10-20 10:55:46 +0200

    Added firebase configuration and basic routes with templates

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+import os
+from dotenv import load_dotenv
+app = Flask(__name__)
+load_dotenv()
+config = {
+  'apiKey': os.getenv('apiKey'),
+  'authDomagit in': os.getenv('authDomain'),
+  'databaseURL': os.getenv('databaseURL'),
+  'projectId': os.getenv('projectId'),
+  'storageBucket': os.getenv('storageBucket'),
+  'messagingSenderId': os.getenv('messagingSenderId'),
+  'appId': os.getenv('appId')
+}
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
