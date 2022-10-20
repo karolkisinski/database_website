537f7b1435b324ce6db1d985b23c1fdc297343a8
karolkisinsi <karol.kisinski@wp.pl>
2022-10-20 11:46:35 +0200

    Configured authorization with firebase

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request, redirect
 import os
 from dotenv import load_dotenv
+import pyrebase
+
 app = Flask(__name__)
+
 load_dotenv()
+
 config = {
   'apiKey': os.getenv('apiKey'),
-  'authDomagit in': os.getenv('authDomain'),
+  'authDomain': os.getenv('authDomain'),
   'databaseURL': os.getenv('databaseURL'),
   'projectId': os.getenv('projectId'),
   'storageBucket': os.getenv('storageBucket'),
@@ -13,8 +17,24 @@ config = {
   'appId': os.getenv('appId')
 }
 
-@app.route('/')
+app.secret_key = os.getenv('SECRET_KEY')
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+
+
+@app.route('/', methods=['POST', 'GET'])
 def home():
+    if('user' in session):
+        return 'Hi, {}'.format(session['user'])
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        try:
+            auth.sign_in_with_email_and_password(email, password)
+            session['user'] = email
+            return "You were logged in successfully"
+        except:
+            return 'Failed to login'
     return render_template('home.html')
 
 @app.route('/login')
@@ -24,6 +44,11 @@ def login():
 @app.route('/register')
 def register():
     return render_template('register.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('user')
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
