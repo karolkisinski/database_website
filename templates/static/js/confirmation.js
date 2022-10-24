1b0f9abf9475d9c92a006982c6f2120207e137f4
karolkisinsi <karol.kisinski@wp.pl>
2022-10-24 13:48:17 +0200

    wip - confirmation modal for add_question form

---
+++ templates/static/js/confirmation.js
@@ -0,0 +1,24 @@
+var myModal = document.getElementById('modal')
+var myInput = document.getElementById('addBtn')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
+
+
+$(document).ready(function () {
+	$( "#addBtn" ).click(function() {
+    if($('#title').val() != null && $('#title').val() != ""){
+        $('#confirmationMsg').html('Are you sure you want to add ('+$('#title').val()+') ?');
+        $('#modal').modal('show');
+      }else{
+        alert('enter a value in the textbox to submit');
+      }
+  });
+  
+  $('#confirmedBtn').click(function(){
+    $('#modal').modal('hide');
+    $( "#question_form" ).submit();
+	});
+  
+});
